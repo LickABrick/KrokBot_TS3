a9b399fe4a9bea031ced9be3db235f65e903917d
LickABrick <guuswikkerink@gmail.com>
2018-10-18 21:58:14 +0200

    Now using python-valve to gather info about the Ark Server

--- main.py
+++ main.py
@@ -1,55 +1,40 @@
 import ts3
-import requests
-import json
+import valve.source.a2s
 import paramiko
 from threading import Thread
 import time
 from datetime import datetime, timedelta
-import re
 import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-hostname = config['Ark Server']['hostname']
-username = config['Ark Server']['username']
-password = config['Ark Server']['password']
+
+def ark_ssh_exec(command):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname=config['Ark Server']['hostname'], username=config['Ark Server']['username'], password=config['Ark Server']['password'])
+    ssh_client.exec_command(command)
 
 
-def ark_get_playercount():
-    if ark_server_status == "online":
-        url = "https://arkservers.net/api/query/{}:27015".format(config['Ark Server']['public_ip'])
-        page = requests.get(url)
-        json_data = json.loads(page.text)
-        return json_data['info']['Players']
-    else:
-        return 0
-
-
-def ark_get_serverstatus():
+def ark_get_serverinfo():
+    global ark_playercount
     global ark_server_status
-    ark_server_status = 'unknown'
+    ark_server_status = "unknown"
+    server_address = (config['Ark Server']['hostname'], int(config['Ark Server']['query_port']))
     while True:
-        command = "timeout -k 30 20 ./arkserver monitor | tail -1"
-        ssh_client = paramiko.SSHClient()
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname=hostname, username=username, password=password)
-
-        stdin, stdout, stderr = ssh_client.exec_command(command)
-        result = stdout.readlines()
-        result = str(result[0]).replace('\n', '')
-        ansi_escape = re.compile(r'\x1b[^m]*m')
-        result = ansi_escape.sub('', result)
-        if "* To enable monitor run ./arkserver start" in result:
+        try:
+            with valve.source.a2s.ServerQuerier(server_address) as server:
+                ark_playercount = server.info()['player_count']
+                ark_server_status = "online"
+        except valve.source.NoResponseError:
+            if ark_server_status == "starting":
+                pass
+            else:
                 ark_server_status = "offline"
-        elif "  OK  ] Monitor arkserver: Querying port: gsquery: {}:27015 : 0/1: OK".format(config['Ark Server']['hostname']) in result:
-            ark_server_status = "online"
-        elif "Monitor arkserver: Querying port: gsquery: {}:27015 : 10/1: WAIT".format(config['Ark Server']['hostname']) in result:
-            ark_server_status = "starting"
-        else:
-            ark_server_status = "error"
-        # print(ark_server_status)
-        ssh_client.close()
+            ark_playercount = 0
+        print(ark_server_status)
+        print("Players online: {}".format(ark_playercount))
         time.sleep(10)
 
 
@@ -57,22 +42,14 @@ def ark_start_server():
     global ark_server_status
     ark_server_status = "starting"
     command = "timeout -k 360 300 ./arkserver start"
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(hostname=hostname, username=username, password=password)
-    ssh_client.exec_command(command)
-    ssh_client.close()
-    # print("Ark Server has been started!")
+    ark_ssh_exec(command)
+    print("Ark Server has been started!")
 
 
 def ark_stop_server():
     command = "./arkserver stop"
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(hostname=hostname, username=username, password=password)
-    ssh_client.exec_command(command)
-    ssh_client.close()
-    # print("Ark Server has been stopped!")
+    ark_ssh_exec(command)
+    print("Ark Server has been stopped!")
 
 
 def ark_get_serveridletime():
@@ -83,12 +60,11 @@ def ark_get_serveridletime():
         if ark_server_status == "starting":
             time_active = datetime.now()
         elif ark_server_status == "online":
-            players_online = ark_get_playercount()
-            if players_online > 0:
+            if ark_playercount > 0:
                 time_active = datetime.now()
             idle_time = datetime.now() - time_active
-            if idle_time > timedelta(minutes=15):
-                print("Server has been idle for over 15 minutes now, stopping server")
+            if idle_time > timedelta(minutes=30):
+                print("Server has been idle for over 30 minutes, stopping server")
                 ark_server_status = "offline"
                 Thread(target=ark_stop_server).start()
             time.sleep(30)
@@ -96,11 +72,10 @@ def ark_get_serveridletime():
             idle_time = "none"
 
 
-thread1 = Thread(target=ark_get_serverstatus, daemon=True)
+thread1 = Thread(target=ark_get_serverinfo, daemon=True)
 thread2 = Thread(target=ark_get_serveridletime, daemon=True)
 thread1.start()
 thread2.start()
-
 
 with ts3.query.TS3Connection(config['TeamSpeak3']['hostname']) as ts3conn:
     # Note, that the client will wait for the response and raise a
@@ -123,23 +98,21 @@ with ts3.query.TS3Connection(config['TeamSpeak3']['hostname']) as ts3conn:
         # Send keepalive message to avoid being kicked after 10 minutes.
         ts3conn.send_keepalive()
         try:
-            # Once every 550 seconds a new keepalive message will be sent.
+            # Once every 100 seconds a new keepalive message will be sent.
             event = ts3conn.wait_for_event(timeout=100)
 
         except ts3.query.TS3TimeoutError as e:
-            pass
-            #print(e)
+            print(e)
         else:
             # Print event to terminal (for testing purposes)
             # print(event[0])
 
             if "msg" in event[0]:
                 if event[0]["msg"].lower() == "!ark status":
-                    playercount = ark_get_playercount()
-                    msg = "The ark server is status is: {} and there are currently {} player(s) online! Server has been idle for {}".format(ark_server_status, playercount, idle_time)
+                    msg = "The ark server is status is: {} and there are currently {} player(s) online! Server has been idle for {}".format(ark_server_status, ark_playercount, idle_time)
                     ts3conn.sendtextmessage(targetmode="2", target=1, msg=msg)
 
-                if event[0]["msg"].lower() == "!ark start":
+                elif event[0]["msg"].lower() == "!ark start":
                     if ark_server_status == "starting":
                         msg = "The ark server is already being started, this can take upto 2 minutes."
                     elif ark_server_status == "online":
@@ -153,8 +126,7 @@ with ts3.query.TS3Connection(config['TeamSpeak3']['hostname']) as ts3conn:
                     else:
                         msg = "Error, please contact an administrator"
                     ts3conn.sendtextmessage(targetmode="2", target=1, msg=msg)
-
-                if event[0]["msg"].lower() == "!help":
+                elif event[0]["msg"].lower() == "!help":
                     msg = "List of commands: \n " \
                           "!help \n" \
                           "!ark status \n" \
